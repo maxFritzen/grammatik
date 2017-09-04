81f91e4bf736ea157c8cff73165ceb9793ffc756
maxFritzen <max.fritzen@hotmail.se>
2017-09-04 18:21:07 +0200

    Add files via upload

--- www/js/javaScript.js
+++ www/js/javaScript.js
@@ -1,26 +1,25 @@
 var wordType = document.getElementById("wordType");
 var wordTypetest = document.getElementById("wordTypetest");
 var listWord = document.getElementsByClassName("listWord");
-var mode;
+var word = ["substantiv", "verb", "adjektiv"];
 var correctAnswers = 0;
+var correctTypeOfWord;
 var currentQuestion = 0;
 var numberOfQuestions = 2;
+var correctWord = []; //för att se de rätta svaren på test, inte de användaren valde.
+var userAnswers = []; //för att kunna visa/lagra användarens svar.
+var randomList = []; //väljer orden som användaren kan välja mellan.
 document.getElementById("numberOfQuestions").innerHTML = numberOfQuestions;
 var addedSelectedClass = false;
 function test(){
     
     if (numberOfQuestions > currentQuestion){
-        reset(); //ifall någon är selected så blir den de-selected, verkar bugga då och då annars.
+        reset(); //ifall någon är selected så blir den de-selected.
         console.log("test");
-        mode ="test";
-        console.log(mode);
         currentQuestion++;
         document.getElementById("currentQuestion").innerHTML = currentQuestion;
         document.getElementById("menu").style.display="none";
         document.getElementById("test").style.display="block";
-        var word = ["substantiv", "verb", "adjektiv"];
-        rand = Math.floor(Math.random() * 3);
-        wordTypetest.innerHTML = word[rand];
         createList("test");
    }
    if(currentQuestion == numberOfQuestions){
@@ -30,20 +29,80 @@ function test(){
         console.log(correctAnswers);
     }
 }
-function addCorrectAnswers(correctAnswer){
-    console.log("correct answer is: " + correctAnswer)
-    //parameter som tar in det rätta svaret
-    //lägger det i en array, som sedan kan skrivas ut. Kan ju använda mig av currentQuestion väl?
-    if (currentQuestion <= numberOfQuestions){
-        console.log("question number" + currentQuestion + " :" + correctAnswer); 
+function checkAnswertest() {
+    //för test.
+    console.log("checkAnswertest");
+    //Kolla vilket ord som har klass "selected".
+    answer = document.querySelector(".selected");
+    storeUserAnswers(answer.innerHTML);
+    
+}
+
+function storeUserAnswers(currentAnswer){
+    userAnswers[currentQuestion-1] = currentAnswer;
+    console.log("usersAnswer : " + userAnswers);
+}
+
+function compareTwoArrays(array1,array2){
+    //Jämför rätta svaren med användarens svar.
+    //ifall rätt svar == användarens svar, plussa på correctAnwsers  
+    for(i = 0; i < array1.length; i++){
+        if(array1[i] == array2[i]){
+            console.log("index" + i +" is the same");
+            correctAnswers++;
+        }
+        else{
+            console.log("index " + i +" is NOT the same");
+        }
     }
-     
+}
+function pickOneWordFromMultipleArrays(x,y,z){
+    //först välj en random av de 3 arrayerna
+    //sedan välj ett random ord ur den valda arrayen
+    arrays = [x,y,z]
+    rand = Math.floor(Math.random() * 3);
+    chosenArray = arrays[rand];
+    rand = Math.floor(Math.random() * x.length);
+    chosenWord = chosenArray[rand];
+    console.log("Randomly Chosen Word will now be:" +chosenWord);
+}
+function decideWordType(randomWord){
+    //parameter tar in ett random ord av subjektiv,adjektiv eller verb (chosenWord)
+    //loopa igenom och kolla ifall indexOf är större än -1 dvs ordet finns i arrayen
+    //är det det, display den sortens wordType
+    //lagra specifika ordet som correctWord.
+    if(substantiv.indexOf(randomWord) > -1){
+        
+        console.log("ordet är ett substantiv:" + randomWord );
+        randomList[0] = randomWord; //istället för att använda index 0 kan jag ju söka igenom och hitta rätt, men funkar for now.
+        correctWord[currentQuestion -1] = randomWord; //-1 för att hamna på index 0
+        wordTypetest.innerHTML = "substantiv";
+    }
+    else if(adjektiv.indexOf(randomWord) > -1){
+       
+        console.log("ordet är ett adjektiv:" + randomWord );
+        randomList[2] = randomWord;
+        correctWord[currentQuestion -1] = randomWord;
+        wordTypetest.innerHTML = "adjektiv";
+    }
+    else if(verb.indexOf(randomWord) > -1){
+        
+        console.log("ordet är ett verb:" + randomWord );
+        randomList[1] = randomWord;
+        correctWord[currentQuestion -1] = randomWord;
+        wordTypetest.innerHTML = "verb";
+    }
+    else{
+        console.log("Ordet hittas inte alls, någonting har gått snett...");
+    }
     
 }
 function showResults(){
+    compareTwoArrays(correctWord,userAnswers);
     resultsMessage = document.getElementById("resultsMessage");
     resultsMessage.innerHTML ="Resultat:" + correctAnswers + "/" +numberOfQuestions;
     resultsMessage.classList.toggle("show");
+    
 }
 
 function resetResults(){
@@ -55,20 +114,16 @@ function resetResults(){
 
 function practice() {
 reset();
-
 console.log("practice");
-mode = "practice";
-console.log(mode);
 document.getElementById("menu").style.display="none";
 document.getElementById("practice").style.display="block";    
-    
-    
-var word = ["substantiv", "verb", "adjektiv"];
+
 rand = Math.floor(Math.random() * 3);
 
 wordType.innerHTML = word[rand];
 createList("practice");
 }
+
 function createList(x){
     addedSelectedClass = false;
     for (var i = 0; i < listWord.length; i++) {
@@ -77,8 +132,9 @@ function createList(x){
     
     if(addedSelectedClass == true){
         document.querySelector(".selected").classList.remove("selected");
-    }
-    //Det här borde ju flyttas utanför funktionen.
+    } 
+    //Det här borde ju flyttas utanför funktionen, men funkar för prototyp
+    
      substantiv = ["Äpple", "Päron", "Bordskiva",
                     "Stege", "Boll","Dator",
                     "Hus", "Bil", "Cykel"];
@@ -88,44 +144,48 @@ function createList(x){
      adjektiv = ["Grön","Stark","Rolig",
                     "Snäll", "Elak", "Mjuk",
                     "Varm", "Kall", "Lång"];
-     //Varför har jag gjort en for-loop här?Behövs väl inte?       
-    //for(i=0; i<3; i++){
-        rand = Math.floor(Math.random() * substantiv.length);
-        var randomList = [substantiv[rand], verb[rand], adjektiv[rand]];
-        //för test, gör jag här ett rätt svar. Kan bygga om övning att följa detta sedan också, om det blir bra.
-        rand = Math.floor(Math.random() * 3);
-        correctAnswer = [randomList[rand]];
-        addCorrectAnswers(correctAnswer);
-        //if det här ordets array är samma som wordTypes innerhtml, lagra det som rätt svar?
-    //}
     
-    //Blanda listan, sen ges ett värde.
+     
+        rand = Math.floor(Math.random() * substantiv.length);
+        randomList = [substantiv[rand], verb[rand], adjektiv[rand]];
+        
+        if(x =="test"){
+            //för test, gör jag här ett rätt svar. Kan bygga om övning att följa detta sedan också, om det blir bra.
+            //kolla vilken array ordet är i.
+            pickOneWordFromMultipleArrays(substantiv, verb, adjektiv);
+            decideWordType(chosenWord);
+            console.log("correctWords : " +correctWord);
+            }
+    
+    //Blanda listan
     randomList.sort(function(a, b){return 0.5 - Math.random()}); 
-    for(i = 1; i<4; i++){
-        rand = Math.floor(Math.random() * 3);
-        if(x=="practice"){
-            listWord = document.getElementById("listWord" + i);      
-            listWord.innerHTML = randomList[i - 1];
+    //Sedan ges ett värde.
+    if(x=="practice"){
+        for(i = 1; i<4; i++){
+                rand = Math.floor(Math.random() * 3);
+                listWord = document.getElementById("listWord" + i);      
+                listWord.innerHTML = randomList[i - 1];
+            }
         }
-        else if(x=="test"){
+    else if(x=="test"){
+        for(i = 1; i<4; i++){
+                rand = Math.floor(Math.random() * 3);
                 listWord = document.getElementById("listWordtest" + i);      
                 listWord.innerHTML = randomList[i - 1];
-                //lagra rätta svaren nu då.
+            }
+        }
                 
-                }
-        
-    }
 }
+
 
 function changeClass() {
     selectedId = document.getElementById(this.id);
-    
-    
+ 
     if (!selectedId.classList.contains("selected")){
         //Ifall det är andra element som är selected, ta bort deras selected.
         if(addedSelectedClass == true){
-        document.querySelector(".selected").classList.remove("selected");
-        }
+            document.querySelector(".selected").classList.remove("selected");
+        } 
         document.getElementById(this.id).className += " selected";
         addedSelectedClass = true;
         
@@ -133,13 +193,10 @@ function changeClass() {
     
 }
 function checkAnswer() {
-    
-    //Kolla vilket id som har klass "selected".
+    //För practice.
+    //Kolla vilket ord som har klass "selected".
     answer = document.querySelector(".selected");
-    
-    
-    //Jämför wordType med listWords array-parent(alltså vilken array den är i.)
-    //kolla array.indexOf, se ifall ordet finns i det. Ifall inte, kolla nästa osv.
+    //Använd array.indexOf, se ifall ordet finns i den arrayen
     //indexOf ger -1  ifall det inte hittar ordet.
     
     if(substantiv.indexOf(answer.innerHTML) >= 0){
@@ -180,67 +237,7 @@ function checkAnswer() {
     }
     
 }
-function checkAnswertest() {
-    console.log("checkAnswertest");
-    //Kolla vilket id som har klass "selected".
-    answer = document.querySelector(".selected");
-    //Kolla vad för sorts ord som ska vara rätt(alltså wordTypetest)
-    console.log("ordsort:" + wordTypetest.innerHTML);
-    correctWordType = wordTypetest.innerHTML;
-    //hitta det ordet som är av rätt typ. tips:nameOfArray.constructor 
-    console.log()
-    //lagra det ordet i en array av rätt svar.
-    
 
-    
-    
-    
-    /*
-    if(substantiv.indexOf(answer.innerHTML) >= 0){
-        
-        //kolla ifall det är rätt
-        if(wordTypetest.innerHTML =="substantiv"){
-            //alertMessage(answer = "right");
-            console.log("correct answer");
-            correctAnswers++;
-            
-        }
-        else{
-            //alertMessage(answer = "wrong");
-            console.log("wrong answer");
-        }
-        
-    }
-    else if(verb.indexOf(answer.innerHTML) >= 0){
-        
-        if(wordTypetest.innerHTML =="verb"){
-            //alertMessage(answer = "right");
-            console.log("correct answer");
-            correctAnswers++;
-        }
-        else{
-            //alertMessage(answer = "wrong");
-            console.log("wrong answer");
-            
-        }
-        
-    }    
-    else if(adjektiv.indexOf(answer.innerHTML) >= 0){
-        
-        if(wordTypetest.innerHTML =="adjektiv"){
-            //alertMessage(answer = "right");
-            console.log("correct answer");
-            correctAnswers++;
-            
-        }
-        else{
-            //alertMessage(answer = "wrong");
-            console.log("wrong answer");
-            
-        }
-    }
-    */
-}
 
 
 
@@ -248,29 +245,18 @@ function alertMessage(answer){
     
     if(answer=="right") {
         document.querySelector(".selected").classList.remove("selected");
-        if(mode=="practice"){
-            message = document.getElementById("success");
-        }
-        else if(mode=="test"){
-            //message = document.getElementById("successtest");
-            
-        }
+        message = document.getElementById("success");
         message.classList.toggle("show");
     }
     else if(answer=="wrong"){
-        if(mode=="practice"){
-            message = document.getElementById("wrong");
-        }
-        else if(mode=="test"){
-            //message = document.getElementById("wrongtest");
-        }
+        message = document.getElementById("wrong");
         message.classList.toggle("show");
     }
 }
 //Visa/dölja rätt/fel-meddelande on click.
-document.getElementById("success").addEventListener("click", myFunction);
-document.getElementById("wrong").addEventListener("click", myFunction);
-function myFunction() {
+document.getElementById("success").addEventListener("click", popUpFunction);
+document.getElementById("wrong").addEventListener("click", popUpFunction);
+function popUpFunction() {
     var popup = document.getElementById(this.id);
     popup.classList.toggle("show");
 }
@@ -288,12 +274,11 @@ function showMenu(){
 }
 function reset(){
 
-//för att lösa bugg som gjorde att det ibland var en eller flera selected.
+//för att det ibland var en eller flera selected.
     if(document.querySelector(".selected")){
         document.querySelector(".selected").classList.remove("selected");
     }
 }
 function resetValues(){
-    //correctAnswers = 0;
     currentQuestion = 0;
 }
